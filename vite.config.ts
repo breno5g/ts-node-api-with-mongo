9df673580b9e36cdafd278c8a7d04e32778f11c0
Breno Santos <brenosantos145@gmail.com>
2022-12-08 08:17:33 -0300

    chore: add ts paths to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 /// <reference types="vitest" />
+import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@controllers': path.resolve(__dirname, 'src/adapters/controllers'),
+      '@middlewares': path.resolve(__dirname, 'src/adapters/middlewares'),
+      '@repositories': path.resolve(__dirname, 'src/adapters/repositories'),
+      '@external': path.resolve(__dirname, 'src/external'),
+      '@entities': path.resolve(__dirname, 'src/entities'),
+      '@services': path.resolve(__dirname, 'src/services'),
+      '@shared': path.resolve(__dirname, 'src/shared')
+    }
+  },
   test: {
     exclude: [
       'node_modules'
